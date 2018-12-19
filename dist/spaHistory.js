e8f3a210da9f04288dc87190e36318f1a6ed27ac
Jiang Fengming <jiangfengming87@gmail.com>
2018-12-19 10:41:41 +0800

    * change package.json `main` field ( https://github.com/webpack/webpack/issues/7482#issuecomment-394838925 ); * upgrade pkgs;

--- dist/spaHistory.js
+++ dist/spaHistory.js
@@ -1,8 +1,8 @@
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
   typeof define === 'function' && define.amd ? define(['exports'], factory) :
-  (factory((global.spaHistory = {})));
-}(this, (function (exports) { 'use strict';
+  factory(global.spaHistory = {});
+}(typeof self !== 'undefined' ? self : this, function (exports) { 'use strict';
 
   function _inheritsLoose(subClass, superClass) {
     subClass.prototype = Object.create(superClass.prototype);
@@ -334,4 +334,4 @@
 
   Object.defineProperty(exports, '__esModule', { value: true });
 
-})));
+}));
